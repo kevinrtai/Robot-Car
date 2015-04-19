61e1118800ecbebad8233926a1474ee2019ae794
kevinrtai <kevin.r.tai@gmail.com>
2015-04-18 19:23:32 -0500

    Some comments

--- testMotor.ino
+++ testMotor.ino
@@ -6,14 +6,19 @@ const int motor2a = 10;
 const int motor2b = 11;
 const int motor2En = 13;
 
+//Initialization function
 void setup() {
+  //Put each pin in the correct mode. In this case, since we're
+  //only controlling motors, we put all pins to ouput mode.
   pinMode(motor1a, OUTPUT); 
   pinMode(motor1b, OUTPUT);
   pinMode(motor2a, OUTPUT);
   pinMode(motor2b, OUTPUT);
 }
 
+//Infinite loop called automatically by the Arduino board
 void loop() {
+  //Digital write writes a value, HIGH or LOW to the output port
   digitalWrite(motor1a, HIGH);
   digitalWrite(motor1b, LOW);
   digitalWrite(motor2a, HIGH);
